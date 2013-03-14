512d4eddc22ba155396ff1f0f3ebbded35a02eaa
Chaudhry Usman Ali <maniali@gmail.com>
2013-03-14 13:31:38 -0300

    first commit

---
+++ tifcounter.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+
+# Imports
+import sys
+import os
+from smtplib import SMTP_SSL as SMTP
+import glob
+
+# Dictionary of Paths
+paths = {
+         'path1':'/home/mani/aptana/Mani/',
+         'path2':'/home/mani/Downloads/TIFF_Files/',
+         'path3':'/home/mani/Downloads/FTT/'
+         }
+
+# Intialize the message
+message = ""
+
+# Iterate items in paths 
+#find display number of files ending with TIF extension
+for key, value in paths.iteritems():
+    message = message + paths[key]+' : '+ str(len(glob.glob(os.path.join(value,'*.TIF')))) + ' Files \n'
+
+# SMTP Configuration    
+SMTP_SERVER = 'smtp.unb.ca'
+SMTP_PORT = 465
+
+sender = 'u4sd0@unb.ca'
+USERNAME = 'u4sd0'
+PASSWORD = 'Indi@611'
+text_subtype = 'plain'
+
+destination = ['maniali@gmail.com']    
+content = "Image Count on the Server: \n" + str(message)
+
+from email.MIMEText import MIMEText
+
+try:
+    msg = MIMEText(content, text_subtype)
+    msg['Subject']=  'Server Status'
+    msg['From']   = sender # some SMTP servers will do this automatically, not all
+
+    conn = SMTP(SMTP_SERVER)
+    conn.set_debuglevel(False)
+    conn.login(USERNAME, PASSWORD)
+    try:
+        conn.sendmail(sender, destination, msg.as_string())
+    finally:
+        conn.close()
+
+except Exception, exc:
+    sys.exit( "mail failed; %s" % str(exc) ) # give a error message
+
